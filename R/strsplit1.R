041cf7f9a5aedc9b498ed4667a48806895c64c4c
hmartin11 <hmartin11@telus.net>
2022-04-02 10:15:14 -0700

    converted roxygen comment in Rd file

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Splits a string
+#'
+#' @param x A character vector with one element
+#' @param split What to split on
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x <- "alpha,beta,delta,omega"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
