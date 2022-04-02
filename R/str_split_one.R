c8a10dc89d2b7d460bfecdda4d7aed008cc009bc
hmartin11 <hmartin11@telus.net>
2022-04-02 11:16:24 -0700

    fixed function param

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,6 +1,6 @@
 #' Splits a string
 #'
-#' @param x A character vector with one element
+#' @param string A character vector with one element
 #' @inheritParams stringr::str_split
 #'
 #' @return A character vector.
